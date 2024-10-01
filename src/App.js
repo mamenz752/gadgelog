42946f1f350f375d38a2063a4e57a0815e8539da
mamenz752 <mamenz752@gmail.com>
2024-10-02 08:39:24 +0900

    fix: FooterをFooterDemoに修正

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Footer from './components/layouts/Footer';
+import FooterDemo from './components/layouts/FooterDemo';
 
 function App() {
   return (
     <div className="App">
-      <Footer />
+      <FooterDemo />
     </div>
   );
 }
---
+++ src/components/layouts/FooterDemo.js
@@ -0,0 +1,11 @@
+import "../../styles/FooterDemo.css";
+
+function FooterDemo() {
+  return (
+    <p>
+      これはフッターです。
+    </p>
+  )
+}
+
+export default FooterDemo;
