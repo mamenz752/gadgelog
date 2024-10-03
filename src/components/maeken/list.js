57d03ebc693dda845b17be92b6a3f6a41c0a839a
mk-619 <pknn9107@yahoo.co.jp>
2024-10-03 09:58:55 +0900

    list.jsの実装

---
+++ src/components/maeken/list.js
@@ -0,0 +1,17 @@
+import "../../styles/lit.css";
+import "../../public/image1.png";
+
+function list(){
+    return(
+        <div>
+            <img src="/image1.png"/>
+            <div>
+                <h1>テキストテキスト</h1>
+                <div>
+                    <h2>テキストテキストテキストテキスト</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default list;
