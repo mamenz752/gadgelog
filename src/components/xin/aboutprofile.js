3a7146ea40fcecf47d69fd5cf932ed0c02353cf8
xinru-3 <2447903037@qq.com>
2024-10-08 15:56:57 +0900

    AboutProfleの実装途中

--- src/components/xin/aboutprofile.js
+++ src/components/xin/aboutprofile.js
@@ -1,10 +1,27 @@
-import "../../styles/xin/aboutprofile.css";
+import xinstyles from "../../styles/xin/aboutprofile.module.css";
+import xinimage from "../../styles/xin/Rectangle.png"
 
 function AboutProfile() {
   return (
-    <p>
-      これはaboutusです。
-    </p>
+    <div>
+      <p className={xinstyles.x}>これはAbout Usです。</p>
+      <div className={xinstyles.media}>
+        <div className={xinstyles.card}>
+          <div className={xinstyles.img}>
+            <img src={xinimage}/>
+          </div>
+          <div>
+            <p className={xinstyles.name}>近代太郎</p>
+            <p>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+          </div>
+        </div>
+
+        {/* <div className={xinstyles.card}>2</div>
+        <div className={xinstyles.card}>3</div>
+        <div className={xinstyles.card}>4</div> */}
+      </div>
+    </div>
+    
   )
 }
 
