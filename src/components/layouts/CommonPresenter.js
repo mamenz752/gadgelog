d50031f70cbe625f22cfeb6e6ca41afd5a218b7e
mamenz752 <mamenz752@gmail.com>
2024-10-29 13:29:34 +0900

    feat: react-router-domのインストール

--- src/components/layouts/CommonPresenter.js
+++ src/components/layouts/CommonPresenter.js
@@ -4,17 +4,18 @@ import Midasi1 from '../hatake/midasi1';
 import Midasi2 from '../hatake/midasi2';
 import Midasi3 from '../hatake/midasi3';
 import ArticleListsPresenter from './ArticleListsPresenter';
+import styles from '../../styles/layouts/Common.module.css';
 
 function CommonPresenter() {
   return (
-    <>
+    <div className={ styles.container }>
       <Hiduke />
       <Ikon />
       <Midasi1 />
       <Midasi2 />
       <Midasi3 />
       <ArticleListsPresenter />
-    </>
+    </div>
   )
 }
 
