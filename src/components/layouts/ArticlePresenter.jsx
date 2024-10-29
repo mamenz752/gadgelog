ed65f178b58b1c02311821db561eb82370a85ad3
mamenz752 <mamenz752@gmail.com>
2024-10-29 13:41:51 +0900

    feat: react-router-domのパス実装

---
+++ src/components/layouts/ArticlePresenter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArticlePresenter = () => {
+  return (
+    <div>ArticlePresenter</div>
+  )
+}
+
+export default ArticlePresenter
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,8 @@
 import CommonPresenter from "../components/layouts/CommonPresenter";
+import ArticlePresenter from "../components/layouts/ArticlePresenter";
 
 export const AppRoutes = [
   { path: "/", element: <CommonPresenter /> },
+  { path: "/about", element: <></> },
+  { path: "/articles/:id", element: <ArticlePresenter />}
 ];
