f7bdc55e4f7b004515ca60bc71884552b3eac01f
mamenz752 <mamenz752@gmail.com>
2024-11-28 12:32:46 +0900

    fix: コンパイルwarningの修正

--- src/components/layouts/ArticlePresenter.jsx
+++ src/components/layouts/ArticlePresenter.jsx
@@ -14,72 +14,45 @@ const ArticlePresenter = () => {
   
   return (
     <div>
-
-      {/* <div className={styles.container}> */}
       <Hiduke
         title={data.title}
         hiduke={data.createdAt}
         />
-      {/* </div> */}
-
-      {/* // <div className={styles.card}>
-      // <div className={styles.image}>
-      // <div className={styles.text}> */}
+        <Midasi3
+        text={data.index}
+        />
+    {
+      data.contents.map((data) => {
+          if (data.heading === 1){
+            return <Midasi2
+            melit={data.title}
+              text={data.description}
+            />
+          } else if(data.heading === 2){
+            return <Midasi2
+            melit={data.title}
+            text={data.description}
+            />
+          } else if (data.heading === 3) {
+            return <Midasi2
+              melit={data.title}
+              text={data.description}
+            />
+          } else if(data.heading === 4){
+            return <Midasi1
+              midashi={data.title}
+              text={data.description}
+            />
+          } else {
+            return <></>
+          }
+})
+      }
       <Ikon
         ProfileImage={data.imagePath}
         NAME={data.name}
         text={data.description}
       />
-      {/* // </div>
-      // </div>
-      // </div> */}
-
-      {/* <div className={styles.box}> */}
-        <Midasi3
-        text={data.index}
-        />
-
-      {/* <div className={styles.section}>
-      <div className={styles.heading}>
-
-      <div className={styles.content}> */}
-    {
-      data.contents.map((data) => {
-        console.log(data);
-          if (data.heading == 1){
-            return <Midasi2
-            melit={data.title}
-              text={data.description}
-            />
-          }
-
-          else if(data.heading == 2){
-            return <Midasi2
-            melit={data.title}
-              text={data.description}
-              />
-          }
-
-          else if(data.heading ==3){
-            return <Midasi2
-            melit={data.title}
-            text={data.description}
-            />    
-          }
-
-          else if(data.heading ==4){
-            return <Midasi1
-              midashi={data.title}
-              text={data.description}
-            />
-          }
-})
-      }
-      {/* </div>
-      </div>
-      </div>
-      </div> */}
-  
     </div>
   )
 }
